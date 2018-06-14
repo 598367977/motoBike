44f7c70efba67a83e89c7205836df219dd116303
598367977 <598367977@qq.com>
2018-06-14 17:55:38 +0800

    项目初始化

---
+++ src/main.js
@@ -0,0 +1,29 @@
+// The Vue build version to load with the `import` command
+// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
+import Vue from 'vue';
+import App from './App';
+import VueRouter from 'vue-router';
+import goods from './components/goods/goods';
+import ratings from './components/ratings/ratings';
+import seller from './components/seller/seller';
+
+Vue.config.productionTip = false;
+
+/* esliusent-disable no-new */
+let app = Vue.extend(App);
+
+let router = new VueRouter({
+  linkActiveClass: 'active'
+});
+
+router.map({
+  '/goods': {
+    component: goods
+  },
+  '/ratings': {
+    component: ratings
+  },
+  '/seller': {
+    component: seller
+  }
+});
